ce0e83500aaad1948679f07f5b12048018c033ae
Bhavya Kawatra <bhavyakawatra6@gmail.com>
2022-07-04 17:04:38 +0530

    LCS function added and improved template

--- 22_ALGO/03_DP/12_LCS_usingDP.cpp
+++ 22_ALGO/03_DP/12_LCS_usingDP.cpp
@@ -8,111 +8,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define mod 1000000007
-#define vp vector<pair<int,int>>
+#define vi vector<int>
+#define vvi vector<vi>
 #define pb push_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define mem(x, y) memset(x, y, sizeof(x))
-#define SP(x) cout << fixed;cout << setprecision(x);
-#define sz(x) (int)x.size()
-#define fast  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define PI 3.14159265358979323846
-#define lb lower_bound
-#define ub upper_bound
-#define bs binary_search
-#define getss(s) getline(cin, s);
-#define in(n) int n;cin >> n;
-#define arr(n) int arr[n];
-#define in2(a, b) int a,b;cin >> a >> b;
-#define in3(a, b, c) int a,b,c;cin >> a >> b >> c;
-#define max_heap(pq) priority_queue <int> pq;
-#define min_heap(pq) priority_queue <int, vector<int>, greater<int> > pq;
-#define sorta(v) sort(v.begin(),v.end());
-#define sortd(v) sort(v.begin(),v.end());
-#define pn(p) cout<<p<<endl;
-#define pt(p) cout<<p<<" ";
-#define pt2(p,q) cout<<p<<" "<<q<<endl;
-#define pt3(p,q,r) cout<<p<<" "<<q<<" "<<r<<endl;
-#define pt4(p,q,r,s) cout<<p<<" "<<q<<" "<<r<<" "<<s<<endl;
-#define vfor(v) for (auto i =v.begin() ; i!=v.end(); i++)
-#define vbfor(v) for (auto i =v.rbegin() ; i!=v.rend(); i++)
+#define sz size()
+#define mahadev ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vfor(v) for (auto itr =v.begin() ; itr!=v.end(); itr++)
 #define ffor(i, a, b) for (int i = a; i < b; i++)
 #define bfor(i, a, b) for (int i = a - 1; i >= b; i--)
-//#define int long long
-int gcd(int a, int b)
-{
-if (b == 0)
-return a;
-return gcd(b, a % b);
-}
-int count_digit(int n)
-{
-    int c = 0;
-    while (n > 0)
-    {
-        c++;
-        n /= 10;
-    }
-    return c;
-}
-void maxi(int &a, int &b)
-{
-    if (a > b){swap(a, b);}
-}
-int mymin(int a, int b, int c)
-{
-    int mini = min(a, c);
-    return min(mini, b);
-}
-int mymax(int a, int b, int c)
-{
-    int big = max(a, c);
-    return max(big, b);
-}
-// first -> decreasing order
-// second -> increasing order
-bool compr(const pair<int, int> &i1, const pair<int, int> i2)
-{
-    if (i1.first > i2.first)
-        return true;
-    if (i1.first == i2.first)
-        return i1.second < i2.second;
-    return false;
-}
+#define all(v) v.begin(),v.end()
+#define pn(p) cout<<p<<endl
 
-
-
-/*-----------------begin---------------*/
-
-void solve()
-{
-    string s,t;
-    cin>>s>>t;
-    int sz=s.size();
-    int tz=t.size();
-    int dp[tz+1][sz+1];
-    mem(dp,0);
-    ffor(j,1,tz+1){
-        ffor(i,1,sz+1){
-            if(t[j-1]==s[i-1])dp[i][j]=1+dp[i-1][j-1];
-            else{
+/*------------------------------------begin------------------------------------*/
+string LCS(string s,string s2){
+    int sk=s.sz;
+    int sk2=s2.sz;
+    vvi dp(sk+1,vi(sk2+1,0));
+    // dp[i][j] -> longest common subseqence when taking string1 length till i
+    // and string2 length till j
+    ffor(i,1,sk+1){
+        ffor(j,1,sk2+1){
+            if(s[i-1]==s2[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+            }else{
                 dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             }
         }
     }
-    cout<<dp[sz][tz];
+    int i=sk,j=sk2;
+    string ans;
+    while(i>0&&j>0){
+        if(dp[i][j]==dp[i][j-1]){j--;continue;}
+        if(dp[i][j]==dp[i-1][j]){i--;continue;}
+        ans.pb(s2[j-1]);
+        i--;j--;
+    }
+    reverse(all(ans));
+    return ans;//contains longest common subsequence!
+    // dp[sk][sk2] or ans.size() -> length of longest common subsequence
 }
-/*-----------------end---------------*/
+
+void solve()
+{
+    string s;cin>>s;
+    string s2;cin>>s2;
+    string lcs=LCS(s,s2);
+    pn(lcs.sz);// length of longest common subsequence
+}
+
+/*-------------------------------------end-------------------------------------*/
 signed main()
 {
-    #ifdef LOCAL
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-    fast;
+    mahadev;
+
     solve();
     return 0;
 }
